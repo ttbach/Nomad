69293b8356e78c2794d588daf5245f42218266c2
Thao Thi Thu Bach <bach22t@mtholyoke.edu>
2016-03-28 19:35:13 -0400

    Removed create_activation_digest from app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
-  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -59,9 +58,5 @@ class User < ActiveRecord::Base
       self.email = email.downcase
     end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+
 end
